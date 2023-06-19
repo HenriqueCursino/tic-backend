e5ec0bc3716d9156ad7f71c5154c0466e5797853
Henrique Cursino <henrique@henrique.cursino>
2023-06-19 09:08:00 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors({
+    origin: '*', // substitua pelo seu domínio permitido
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // métodos HTTP permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // cabeçalhos permitidos
+  });
+  
+
   await app.listen(3001);
 }
 bootstrap();
