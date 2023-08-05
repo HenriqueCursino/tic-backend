ea6eba4b85581058eaa52b49451804b4e64352ac
HenriqueCursino <henriquecursino46@gmail.com>
2023-08-05 10:35:35 -0300

    fix: move health folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,8 @@ import { OriginModule } from './modules/origin/origin.module';
 import { ProductModule } from './modules/products/products.module';
 import { RoomModule } from './modules/rooms/rooms.module';
 import { UsersModule } from './modules/users/users.module';
-import { HealthService } from './health/health.service';
-import { HealthController } from './health/health.controller';
+import { HealthService } from './modules/health/health.service';
+import { HealthController } from './modules/health/health.controller';
 
 @Module({
   imports: [UsersModule, RoomModule, OriginModule, RoomCategory, ProductModule],
